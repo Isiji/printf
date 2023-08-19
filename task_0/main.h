e9b3b668f1cd0d35a7e84114739a6dd831eef01e
isiji <blairisiji@gmail.com>
2023-08-19 15:19:16 +0000

    printf function

---
+++ task_0/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#endif
---
+++ task_0/printf.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+
+int _printf(const char *format, ...)
+{
+	int char_printed = 0;
+
+	va_list(file);
+
+	if (*format == NULL)
+		return (-1);
+	va_start(file, format);
+	
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_printed++;
+		}
+		else
+		{
+			format++;
+		if (*format == '\0')
+			break;
+
+		if (*format == '%')
+		{
+			write(1, format, 1);
+			char_printed++;
+		}
+		else if (*format == 'c')
+		{
+			char c = va_arg(format, int);
+			write(1, format, 1);
+			char_printed++;
+		}
+		else if (*format == 's')
+		{
+			char *string = va+arg(format, char*);
+			
+			int str_len = 0;
+
+			while (string(str_len != '\0'))
+				str_len++;
+			write(1, format, str_len);
+			char_printed +=str_len;
+		}
+		}
+		format++;
+	}
+	va_end(file);
+
+	return (char_printed);
+
+}
