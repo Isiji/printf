e6c9d6729ae21ad40f0b3d0e77680bc9957056ab
isiji <blairisiji@gmail.com>
2023-08-20 15:43:30 +0000

    printf

--- task_0/printf.c
+++ task_0/printf.c
@@ -10,10 +10,11 @@ int _printf(const char *format, ...)
 {
 	int char_printed = 0;
 
-	va_list(file);
+	va_list file;
 
-	if (*format == NULL)
+	if (format == NULL)
 		return (-1);
+
 	va_start(file, format);
 	
 	while (*format)
@@ -26,6 +27,7 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
+
 		if (*format == '\0')
 			break;
 
@@ -36,20 +38,21 @@ int _printf(const char *format, ...)
 		}
 		else if (*format == 'c')
 		{
-			char c = va_arg(format, int);
-			write(1, format, 1);
+			char c = va_arg(file, int);
+			write(1, &c, 1);
 			char_printed++;
 		}
 		else if (*format == 's')
 		{
-			char *string = va+arg(format, char*);
+			char *string = va_arg(file, char*);
 			
 			int str_len = 0;
 
-			while (string(str_len != '\0'))
+			while (string[str_len] != '\0')
 				str_len++;
-			write(1, format, str_len);
-			char_printed +=str_len;
+
+			write(1, string, str_len);
+			char_printed += str_len;
 		}
 		}
 		format++;
