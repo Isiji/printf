0ef92ff4023699d032680fb20824e7dd83d951a5
isiji <blairisiji@gmail.com>
2023-08-21 07:45:31 +0000

    funtions

---
+++ funct0.c
@@ -0,0 +1,68 @@
+
+
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - modified Printf function
+ * @format: where the string is
+ * Return: Printed characters
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, prchars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list file;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(file, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			prchars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, file);
+			precision = get_precision(format, &i, file);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, file, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			prchars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(file);
+
+	return (prchars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
