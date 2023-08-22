0cdb3efec21475984e8a6957797729212b9f35a5
isiji <blairisiji@gmail.com>
2023-08-22 21:43:57 +0000

    test

--- _printf.c
+++ _printf.c
@@ -12,13 +12,13 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	va_list file;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(file, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -34,11 +34,11 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
+			width = get_width(format, &i, file);
+			precision = get_precision(format, &i, file);
 			size = csize(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_print(format, &i, file, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -48,13 +48,13 @@ int _printf(const char *format, ...)
 
 	print_buffer(buffer, &buff_ind);
 
-	va_end(list);
+	va_end(file);
 
 	return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer.
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
