b4b20011c054cb3d1d929d89a03640df8b502a9e
isiji <blairisiji@gmail.com>
2023-08-22 21:26:46 +0000

    test

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
@@ -11,33 +12,33 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list file;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(file, format);
+	va_start(list, format);
 
-	for (i = 0; format && *format != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = *format;
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-
+			
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, file);
-			precision = get_precision(format, &i, file);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = csize(format, &i);
 			++i;
-			printed = handle_print(format, &i, file, buffer,
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -47,7 +48,7 @@ int _printf(const char *format, ...)
 
 	print_buffer(buffer, &buff_ind);
 
-	va_end(file);
+	va_end(list);
 
 	return (printed_chars);
 }
